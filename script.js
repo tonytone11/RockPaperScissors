90e5526506abd9cf9a350b5b2e97d6730bad82d9
Anthony Montesdeoca <anthony_chen97@yahoo.com>
2024-11-02 12:20:46 -0400

    added rules, how 2 play, & display winner in js

--- script.js
+++ script.js
@@ -17,3 +17,35 @@ function getComputerChoice() {
     return fighters[randomIndex];
 }
 
+// rules to determine the winner
+function winnerRound(playerChoice, computerChoice) {
+    if (playerChoice === computerChoice) {
+        return "DRAW";
+    } else if (
+        (playerChoice === "rock" && computerChoice === "scissors") ||
+        (playerChoice === "paper" && computerChoice === "rock") ||
+        (playerChoice === "scissors" && computerChoice === "paper")
+    ) {
+        return "Winner: User";
+    } else {
+        return "Winner: Opponent";
+    }
+}
+
+// how to play one or each round
+function eachRound(playerChoice) {
+    const computerChoice = getComputerChoice();
+    const winner = winnerRound(playerChoice, computerChoice);
+}
+
+// displaying the fighter and who won
+if (winner === "Winner: User") {
+    playerScore++;
+    winnerDisplay.textContent = `You chose ${playerChoice}, computer chose ${computerChoice}. You won this round!`;
+} else if (winner === "Winner: Opponent") {
+    computerScore++;
+    winnerDisplay.textContent = `You chose ${playerChoice}, computer chose ${computerChoice}. Your opponent won this round!`;
+} else {
+    draws++;
+    winnerDisplay.textContent = `Both chose ${playerChoice}. It's a draw`;
+}
