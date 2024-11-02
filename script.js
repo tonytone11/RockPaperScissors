00f837c59fd7d849cbd88c48e00384f0ed460dfe
Anthony Montesdeoca <anthony_chen97@yahoo.com>
2024-11-02 11:59:25 -0400

    starting js code

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-const match = () => {
+// elements that will display scores and results
+const playerDisplay = document.querySelector('.player1Score');
+const drawDisplay = document.querySelector('.drawScore');
+const competitorDisplay = document.querySelector('.competitorScore');
+const winnerDisplay = document.querySelector('.winner-result');
+const resetBtn = document.querySelector('.again');
 
+// scores 
+let playerScore = 0;
+let computerScore = 0;
+let draws = 0
+
+// computer's fighting options
+function getComputerChoice() {
+    const fighters = ['rock', 'paper', 'scissors'];
+    const randomIndex = Math.floor(Math.random() * fighters.length);
+    return fighters[randomIndex];
 }
+
